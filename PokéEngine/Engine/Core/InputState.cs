efd8073fe3f1e0f8cb9eae2600edfeb1e44853ce
nate0001 <nate0001@a0484e9e-a164-0145-f047-3e459006b046>
2010-09-22 23:41:29 +0000

    First commit.

---
+++ PokéEngine/Engine/Core/InputState.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pokemon.Core
+{
+    public class InputState
+    {
+        public readonly Dictionary<string, Keys> KeyMap = new Dictionary<string, Keys>();
+        public KeyboardState CurrentKeyboardState;
+        public KeyboardState LastKeyboardState;
+
+        public InputState()
+        {
+            CurrentKeyboardState = new KeyboardState();
+            LastKeyboardState = new KeyboardState();
+        }
+
+        public bool KeyRepeat { get; set; }
+
+        public void Initialize()
+        {
+            IEnumerable keys = from key in XElement.Load("Config/input.xml").Elements("Key") select key;
+
+            foreach (XElement key in keys)
+            {
+                XAttribute xAttribute = key.Attribute("name");
+                if (xAttribute != null) KeyMap.Add(xAttribute.Value, (Keys) Enum.Parse(typeof (Keys), key.Value));
+            }
+        }
+
+        public void Update()
+        {
+            LastKeyboardState = CurrentKeyboardState;
+            CurrentKeyboardState = Keyboard.GetState();
+        }
+
+        public bool IsKeyPressed(Keys key)
+        {
+            if (KeyRepeat)
+                return CurrentKeyboardState.IsKeyDown(key);
+
+            return (CurrentKeyboardState.IsKeyDown(key) &&
+                    LastKeyboardState.IsKeyUp(key));
+        }
+
+        public bool IsStartPressed()
+        {
+            return IsKeyPressed(KeyMap["Start"]);
+        }
+
+        public bool IsSelectPressed()
+        {
+            return IsKeyPressed(KeyMap["Select"]);
+        }
+
+        public bool IsAPressed()
+        {
+            return IsKeyPressed(KeyMap["A"]);
+        }
+
+        public bool IsBPressed()
+        {
+            return IsKeyPressed(KeyMap["B"]);
+        }
+
+        public bool IsXPressed()
+        {
+            return IsKeyPressed(KeyMap["X"]);
+        }
+
+        public bool IsYPressed()
+        {
+            return IsKeyPressed(KeyMap["Y"]);
+        }
+
+        public bool IsLPressed()
+        {
+            return IsKeyPressed(KeyMap["L"]);
+        }
+
+        public bool IsRPressed()
+        {
+            return IsKeyPressed(KeyMap["R"]);
+        }
+
+        public bool IsUpPressed()
+        {
+            return IsKeyPressed(KeyMap["Up"]);
+        }
+
+        public bool IsDownPressed()
+        {
+            return IsKeyPressed(KeyMap["Down"]);
+        }
+
+        public bool IsLeftPressed()
+        {
+            return IsKeyPressed(KeyMap["Left"]);
+        }
+
+        public bool IsRightPressed()
+        {
+            return IsKeyPressed(KeyMap["Right"]);
+        }
+    }
+}
---
+++ PokéEngine/Engine/Screens/TouchScreen.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Pokemon.Core;
+
+namespace Pokemon.Screens
+{
+    internal class TouchScreen : GameScreen
+    {
+        private ContentManager _content;
+        private Texture2D _texture2D;
+
+        public TouchScreen()
+        {
+            TransitionOnTime = TimeSpan.FromSeconds(0);
+            TransitionOffTime = TimeSpan.FromSeconds(0);
+        }
+
+        public override void LoadContent()
+        {
+            if (_content == null)
+                _content = new ContentManager(ScreenManager.Game.Services, "Content");
+
+            _texture2D = _content.Load<Texture2D>("bottom");
+
+            ScreenManager.Game.ResetElapsedTime();
+        }
+
+        public override void UnloadContent()
+        {
+            _content.Unload();
+        }
+
+        public override void HandleInput(InputState input)
+        {
+            if (input == null)
+                throw new ArgumentNullException("input");
+
+            if (input.IsStartPressed())
+            {
+                ScreenState = ScreenState.InActive;
+                foreach (var screen in ScreenManager.GetScreens().OfType<OverworldScreen>())
+                {
+                    screen.ScreenState = ScreenState.Active;
+                }
+            }
+        }
+
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus,
+                                    bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, false);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            Viewport viewport = ScreenManager.BottomViewPort;
+
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(_texture2D, new Rectangle(viewport.X, viewport.Y, viewport.Width, viewport.Height),
+                             Color.White);
+            spriteBatch.End();
+        }
+    }
+}
