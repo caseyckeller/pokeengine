2a9857d878201c58cbb947c7237d251a781627c3
darkkal999 <darkkal999@a0484e9e-a164-0145-f047-3e459006b046>
2010-12-11 11:21:23 +0000

    In the process of refactoring the Tile and Zone class

---
+++ trunk/IAPL_Engine/IAPL_Engine/IAPL_Engine/Zones/.wave-vs.net.backup/Tile._11_12_2010_2_50_32_.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IAPL.Map
+{
+    public enum TType { Black, Grass, Dirt, Sand, Brick }; //TODO add more tile types
+    public enum Direction { North, East, South, West };
+    
+    public class Tile
+    {
+        private bool jumpable; //whether the tile is jumpable, like a ledge
+        private AFrom aDirection; //which directions you may enter the tile from
+        private bool anyEvent; //whether any events are on this tile
+        private bool randomEncounter; //whether random encounters can occur on this tile
+        private bool ramp; //whether the tile is a bike ramp
+        private bool occupied;
+
+        //Note, X, Y, and Z must be global in relation to all other tiles of the map
+        //each tile on the map must have a unique combination of X, Y and Z
+        //TODO check if we need these or if we can give this to Zone
+        //private int X; //global X coordinate of tile
+        //private int Y; //global Y coordinate of tile
+        private byte Z; //global Z coordinate of tile
+
+        #region Constructors
+        //default is inaccessable tile
+        public Tile()
+        {
+            jumpable = false;
+            aDirection = new AFrom(false, false, false, false);
+            anyEvent = false;
+            randomEncounter = false;
+            ramp = false;
+            occupied = false;
+
+            //MUST BE MADE UNIQUE
+            //X = 0;
+            //Y = 0;
+            Z = 0;
+        }
+
+        //constructor with given x,y, and z coords
+        //public Tile(int inX, int inY, int inZ)
+        public Tile(int inZ)
+        {
+            jumpable = false;
+            aDirection = new AFrom(false, false, false, false);
+            anyEvent = false;
+            randomEncounter = false;
+            ramp = false;
+            occupied = false;
+
+            //MUST BE MADE UNIQUE
+            //X = inX;
+            //Y = inY;
+            Z = (byte)inZ;
+        }
+        #endregion
+
+        #region Setters
+        public void setJumpable(bool value)
+        {
+            jumpable = value;
+        }
+
+        public void setAccessible(bool inNorth, bool inEast, bool inSouth, bool inWest)
+        {
+            aDirection.north = inNorth;
+            aDirection.east = inEast;
+            aDirection.south = inSouth;
+            aDirection.west = inWest;
+        }
+
+        public void setEvent(bool value)
+        {
+            anyEvent = value;
+        }
+
+        public void setRandomEncounter(bool value)
+        {
+            randomEncounter = value;
+        }
+
+        public void setRamp(bool value)
+        {
+            ramp = value;
+        }
+
+        public void setOccupied(bool value)
+        {
+            occupied = value;
+        }
+        #endregion
+
+        //makes accessible from all directions
+        public void setClear()
+        {
+            aDirection = new AFrom(true, true, true, true);
+        }
+
+        #region Getters
+
+        public bool isJumpable()
+        {
+            return jumpable;
+        }
+
+        public bool isAccessibleFrom(Direction dir)
+        {
+            bool isAc;
+            if(dir == Direction.North && !occupied)
+            {
+                isAc = aDirection.north;
+            }
+            else if (dir == Direction.East && !occupied)
+            {
+                isAc = aDirection.east;
+            }
+            else if (dir == Direction.South && !occupied)
+            {
+                isAc = aDirection.south;
+            }
+            else if (dir == Direction.West && !occupied)
+            {
+                isAc = aDirection.west;
+            }
+            else
+            {
+                isAc = false;
+            }
+
+            return isAc;
+        }
+
+        public bool isAccessibleFrom(String dir)
+        {
+            bool isAc;
+            if (dir == "UP")
+            {
+                isAc = aDirection.north;
+            }
+            else if (dir == "RIGHT")
+            {
+                isAc = aDirection.east;
+            }
+            else if (dir == "DOWN")
+            {
+                isAc = aDirection.south;
+            }
+            else if (dir == "LEFT")
+            {
+                isAc = aDirection.west;
+            }
+            else
+            {
+                isAc = false;
+            }
+
+            return isAc;
+        }
+
+        public bool hasEvent()
+        {
+            return anyEvent;
+        }
+
+        public bool hasRandomEncounter()
+        {
+            return randomEncounter;
+        }
+
+        public bool isRamp()
+        {
+            return ramp;
+        }
+        #endregion
+
+        public bool isOccupied()
+        {
+            return occupied;
+        }
+
+        public bool isClear()
+        {
+            bool val = false;
+            if (aDirection.Equals(new AFrom(true, true, true, true)) && !occupied)
+                val = true;
+            return val;
+        }
+
+    }
+
+    public struct AFrom
+    {
+        //if any of these are set to false it means you may not enter the
+        //tile from that direction
+        //if all are set to false it is an inaccessible tile
+        public bool north;
+        public bool east;
+        public bool south;
+        public bool west;
+
+        
+        public AFrom(bool inNorth, bool inEast, bool inSouth, bool inWest)
+        {
+            north = inNorth;
+            east = inEast;
+            south = inSouth;
+            west = inWest;
+        }
+
+       
+    }
+}
---
+++ trunk/IAPL_Engine/IAPL_Engine/IAPL_Engine/Zones/.wave-vs.net.backup/Zone._11_12_2010_2_50_39_.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IAPL.Map
+{
+    public class Zone
+    {
+        public struct point
+        {
+            public int X;
+            public int Y;
+
+            public point(int inX, int inY)
+            {
+                X = inX;
+                Y = inY;
+            }
+        }
+
+        private String zoneName;  //this is the name of the zone, generally "route xxxx"
+        //TODO make way to store adjacent zones
+        public Tile[,] tile; //a 2D array of nodes, these are individual tiles of a map
+        //starts at 0,0 for bottom left corner and is x,y
+        //so tile[1][2] is the second tile from the left, and 3 from the bottom
+        //NOT global, relative to zone only
+        //global coordinate would be global X and Y plus tile x and y
+
+        //private Scenery scenery;
+
+        private int globalX; //global X coordinate of tile [0][0]
+        private int globalY; //global Y coordinate of tile [0][0]
+
+        
+
+        #region Constructors
+        //default constructor makes a 50x50 zone
+        public Zone()
+        {
+            zoneName = "Default Name";
+            //these must be changed to global unless only one zone exists
+            globalX = 0;
+            globalY = 0;
+            tile = new Tile[50, 50];
+            allToDefault();
+
+        }
+
+        //constructor when given size
+        public Zone(int inX, int inY)
+        {
+            zoneName = "Default Name";
+            //these must be changed to global unless only one zone exists
+            globalX = 0;
+            globalY = 0;
+            tile = new Tile[inX, inY];
+            allToDefault();
+
+        }
+
+        //constructor when given global X and Y coordinates and size
+        public Zone(int inX, int inY, int inXCoord, int inYCoord)
+        {
+            zoneName = "Default Name";
+            globalX = inXCoord;
+            globalY = inYCoord;
+            tile = new Tile[inX, inY];
+            allToDefault();
+
+        }
+        #endregion
+
+        #region Getters
+        public int mapWidth
+        {
+            get { return tile.GetLength(0); }
+        }
+
+        public int mapHeight
+        {
+            get { return tile.GetLength(1); }
+        }
+        #endregion
+
+        private void allToDefault()
+        {
+            for (int a = 0; a < mapWidth; a++)
+            {
+                for (int b = 0; b < mapHeight; b++)
+                {
+                    tile[a, b] = new Tile();
+                }
+            }
+        }
+
+        //returns global coords of tile x y
+        public point convertToGlobal(int x, int y)
+        {
+            point xy = new point(x + globalX, y + globalY);
+            return xy;
+        }
+
+        //returns local coords of tile x y
+        //returns -1,-1 if not on this zone
+        public point convertToLocal(int x, int y)
+        {
+            point xy = new point(-1, -1);
+            if(x < (globalX+mapWidth) && (x >= globalX))
+            {
+                if(y < (globalY+mapHeight) && (y >= globalY))
+                {
+                    xy.X = x - globalX;
+                    xy.Y = y - globalY;
+                }
+            }
+            return xy;
+        }
+
+        public void setMap()
+        {
+            for (int a = 5; a <= 15; a++)
+            {
+                for (int b = 5; b <= 10; b++)
+                {
+                    tile[a, b].setClear();
+                }
+            }
+        }
+
+
+    }
+}
