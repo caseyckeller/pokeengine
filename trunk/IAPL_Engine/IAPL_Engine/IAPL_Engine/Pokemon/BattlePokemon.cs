91d849248d2dfc3be171bdf8b63f6792ef0653af
whatsapokemon <whatsapokemon@a0484e9e-a164-0145-f047-3e459006b046>
2010-12-15 13:54:22 +0000

    Finishing off the BattlePokemon class, also wrote out type strengths/weaknesses, and hidden power stuff

--- trunk/IAPL_Engine/IAPL_Engine/IAPL_Engine/Pokemon/BattlePokemon.cs
+++ trunk/IAPL_Engine/IAPL_Engine/IAPL_Engine/Pokemon/BattlePokemon.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using IAPL.Pokemon;
 using IAPL.Moves;
+using IAPL.Moves.TypeStrengths;
 
 namespace IAPL.Pokemon
 {
@@ -15,6 +16,7 @@ namespace IAPL.Pokemon
     {
         #region class_fields
         public ActivePokemon pokemon;
+        //volatile and battle statuses
         public int confusedRemaining; // number of turns left till confusion wears off automatically, -1 for NA
         public bool cursed; // whether the pokemon is cursed (1/4 health removed every turn)
         public bool flinched; // whether the pokemon has flinched
@@ -29,6 +31,17 @@ namespace IAPL.Pokemon
         public int perishSongRemaining; // number of turns till perish song kills pokemon, -1 for NA
         public int tauntRemaining; //number of turns till taunt wears off, -1 for NA
         public bool tormented; //whether the pokemon is tormented, may not use same move twice in a row
+
+        public bool defenseCurled; //whether the pokemon is defense curled, rollout and iceball damage doubled
+        public bool focusEnergy; //quadruples critical strike rate
+        public bool minimized; //minimize doubles damage taken from Hard Roller and Stomp
+        public bool misted; //whether the pokemon is misted, prevents stat changes
+        public int reflectRemaining; //how many turns reflect has left, -1 for irrelevant
+        public int lightScreenRemaining; //how many turns light screen has left, -1 for irrelevant
+        public int substituteHealth; //the health of the pokemon's substitue, -1 for no substitute
+        public bool trapped; //whether the pokemon is trapped, ie can not switch or run
+        public bool safeguarded; //if true then the move that turn is ineffective
+
 
         private int atk;
         public double atkModifier = 1.0;
@@ -179,6 +192,8 @@ namespace IAPL.Pokemon
                 pokemon.currentHP = value;
                 if (pokemon.currentHP > pokemon.HP)
                     pokemon.currentHP = pokemon.HP;
+                else if (pokemon.currentHP < 0)
+                    pokemon.currentHP = 0;
             }
  
         } //get or set the current health of the pokemon
@@ -212,6 +227,16 @@ namespace IAPL.Pokemon
             tauntRemaining = -1;
             tormented = false;
 
+            defenseCurled = false;
+            focusEnergy = false;
+            minimized = false;
+            misted = false;
+            reflectRemaining = -1;
+            lightScreenRemaining = -1;
+            substituteHealth = -1;
+            trapped = false;
+            safeguarded = false;
+
             attackLevel = 0;
             defenseLevel = 0;
             spAtkLevel = 0;
@@ -221,10 +246,137 @@ namespace IAPL.Pokemon
             accuracyLevel = 0;
         }
 
+        #region inflict_status
+        public void burn()
+        {
+            if(pokemon.status == MajorStatus.None)
+            {
+                pokemon.status = MajorStatus.Burned;
+            }
+        }
+
+        public void freeze()
+        {
+            if (pokemon.status == MajorStatus.None)
+            {
+                pokemon.status = MajorStatus.Frozen;
+            }
+        }
+
+        public void paralyze()
+        {
+            if (pokemon.status == MajorStatus.None)
+            {
+                pokemon.status = MajorStatus.Paralyzed;
+            }
+        }
+
+        public void poison()
+        {
+            if (pokemon.status == MajorStatus.None)
+            {
+                pokemon.status = MajorStatus.Poisoned;
+            }
+        }
+
+        public void badPoison()
+        {
+            if (pokemon.status == MajorStatus.None)
+            {
+                pokemon.status = MajorStatus.BadPoisoned;
+            }
+        }
+
+        public void sleep()
+        {
+            if (pokemon.status == MajorStatus.None)
+            {
+                pokemon.status = MajorStatus.Sleep;
+            }
+        }
+
+        /// <summary>
+        /// confuses for 4 turns, may wear off earlier
+        /// </summary>
+        public void confuse()
+        {
+            confusedRemaining = 4;
+        }
+
+        public void curse()
+        {
+            cursed = true;
+        }
+
+        public void flinch()
+        {
+            //should wear off at start of next turn
+            flinched = true;
+        }
+
+        public void identify()
+        {
+            identified = true;
+        }
+
+        public void hitGhost()
+        {
+            canHitGhost = true;
+        }
+
+        public void hitDark()
+        {
+            canHitDark = true;
+        }
+
+        public void infatuate()
+        {
+            infatuated = true;
+        }
+
+        public void leech()
+        {
+            leechSeeded = true;
+        }
+
+        public void lockOn()
+        {
+            definiteHit = true;
+        }
+
+        public void partiallyTrap()
+        {
+            Random random = new Random(Convert.ToInt32(DateTime.Now.Ticks));
+            int val = random.Next(4 - 5);
+            partialTrapRemaining = val;
+        }
+
+        public void partiallyTrap(bool hasExtendingItem)
+        {
+            if (hasExtendingItem)
+            {
+                partialTrapRemaining = 5;
+            }
+        }
+
         public void perish()
         {
             perishSongRemaining = 3;
         }
+
+        public void taunt()
+        {
+            Random random = new Random(Convert.ToInt32(DateTime.Now.Ticks));
+            int val = random.Next(2 - 4);
+            tauntRemaining = val;
+        }
+
+        public void torment()
+        {
+            tormented = true;
+        }
+
+        #endregion //inflict various volatile and nonvolatile status effects
 
         /// <summary>
         /// returns a double representing the chance to hit this pokemon
@@ -238,6 +390,7 @@ namespace IAPL.Pokemon
             double chance = 0.0;
             double acc = 0.0;
             double eva = 0.0;
+            //convert evasion and accuracy levels into actual values
             if (accuracyLevel >= 0)
             {
                 acc = (Convert.ToDouble(inPoke.accuracyLevel) + 3.0) / 3.0;
@@ -270,6 +423,148 @@ namespace IAPL.Pokemon
             }
             return chance;
         }
+        
+        /// <summary>
+        /// Whether this pokemon hits the target pokemon
+        /// </summary>
+        /// <param name="attack">attack used</param>
+        /// <param name="target">target pokemon</param>
+        /// <returns></returns>
+        public bool hits( ActiveMove attack, BattlePokemon target)
+        {
+            bool doesHit = false;
+
+            double chance = target.chanceToHit(this, attack.bMove);
+            if(chance < 1.0 && chance > 0.0)
+            {
+                Random random = new Random(Convert.ToInt32(DateTime.Now.Ticks));
+                double roll = random.NextDouble();
+                if (roll < chance)
+                {
+                    doesHit = true;
+                }
+                else
+                    doesHit = false;
+            }
+            //chance greater than 1 will always hit
+            else if (chance >= 1.0)
+            {
+                doesHit = true;
+            }
+            //chance less than 0 will never hit
+            else
+            {
+                doesHit = false;
+            }
+
+            return doesHit;
+        }
+
+        /// <summary>
+        /// does damage to the target based on stats and attack power and type and also STAB
+        /// </summary>
+        /// <param name="attack"></param>
+        /// <param name="target"></param>
+        public void doDamageTo( ActiveMove attack, BattlePokemon target)
+        {
+            double dDamage = 0.0;
+            int damage = 0;
+            double strength = typeModifier(attack, target);
+            double stab = 1.0;
+            
+            //calculate Same Type Attack Bonus
+            if (attack.bMove.moveType == pokemon.BasePoke.baseTypeOne.ToString() || attack.bMove.moveType == pokemon.BasePoke.baseTypeTwo.ToString())
+            {
+                stab = 1.5;
+            }
+
+            if (strength > 0.0)
+            {
+                Random random = new Random(Convert.ToInt32(DateTime.Now.Ticks));
+                double modifier = Convert.ToDouble(random.Next(85, 100)) / 100.0;
+                if(attack.bMove.moveKind == "Special")
+                {
+                    dDamage = stab * strength * modifier * (((2.0 * pokemon.level + 10.0) / 250.0) * (Convert.ToDouble(effectiveSpAtk) / Convert.ToDouble(target.effectiveSpDef)) * attack.bMove.power + 2);
+                    damage = Convert.ToInt32(Math.Floor(dDamage));
+                    target.currentHealth -= damage;
+                }
+                else if(attack.bMove.moveKind == "Physical")
+                {
+                    dDamage = stab * strength * modifier * (((2.0 * pokemon.level + 10.0) / 250.0) * (Convert.ToDouble(effectiveAtk) / Convert.ToDouble(target.effectiveDef)) * attack.bMove.power + 2);
+                    damage = Convert.ToInt32(Math.Floor(dDamage));
+                    target.currentHealth -= damage;
+                }
+
+                //show message saying it hit, or was supereffective
+            }
+            else
+            {
+                //show message saying it was ineffective
+            }
+        }
+
+        /// <summary>
+        /// Does damage to the target based on stats and attack power and hidden move type and also STAB
+        /// </summary>
+        /// <param name="attack"></param>
+        /// <param name="target"></param>
+        public void doHiddenPowerDamageTo(ActiveMove attack, BattlePokemon target)
+        {
+            double dDamage = 0.0;
+            int damage = 0;
+            //only this part changes for hidden power
+            double strength = TypeStrengths.typeStrength(pokemon.hiddenPowerType, target);
+            double stab = 1.0;
+
+            //calculate Same Type Attack Bonus
+            if (attack.bMove.moveType == pokemon.BasePoke.baseTypeOne.ToString() || attack.bMove.moveType == pokemon.BasePoke.baseTypeTwo.ToString())
+            {
+                stab = 1.5;
+            }
+
+            if (strength > 0.0)
+            {
+                Random random = new Random(Convert.ToInt32(DateTime.Now.Ticks));
+                double modifier = Convert.ToDouble(random.Next(85, 100)) / 100.0;
+                if (attack.bMove.moveKind == "Special")
+                {
+                    dDamage = stab * strength * modifier * (((2.0 * pokemon.level + 10.0) / 250.0) * (Convert.ToDouble(effectiveSpAtk) / Convert.ToDouble(target.effectiveSpDef)) * pokemon.hiddenPowerPower + 2);
+                    damage = Convert.ToInt32(Math.Floor(dDamage));
+                    target.currentHealth -= damage;
+                }
+                //I know that hidden power is only special, but screw you!
+                else if (attack.bMove.moveKind == "Physical")
+                {
+                    dDamage = stab * strength * modifier * (((2.0 * pokemon.level + 10.0) / 250.0) * (Convert.ToDouble(effectiveAtk) / Convert.ToDouble(target.effectiveDef)) * pokemon.hiddenPowerPower + 2);
+                    damage = Convert.ToInt32(Math.Floor(dDamage));
+                    target.currentHealth -= damage;
+                }
+
+                //show message saying it hit, or was supereffective
+            }
+            else
+            {
+                //show message saying it was ineffective
+            }
+        }
+
+        /// <summary>
+        /// do a set amount of damage to a target
+        /// </summary>
+        /// <param name="amount">amount of damage to be done</param>
+        /// <param name="target">target pokemon</param>
+        public void doSetDamage(int amount, BattlePokemon target)
+        {
+            target.currentHealth -= amount;
+
+            //show message saying it hit
+        }
+
+        public double typeModifier( ActiveMove attack, BattlePokemon target)
+        {
+            return TypeStrengths.typeStrength(attack, target);         
+        }
+
 
         /// <summary>
         /// returns percentage that stat is affected by stat increases and decreases
