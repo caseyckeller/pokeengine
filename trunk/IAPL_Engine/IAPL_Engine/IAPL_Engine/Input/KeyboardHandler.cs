2ba559ddfe6a291ca286165d834a7fa83ef3cfe5
darkkal999 <darkkal999@a0484e9e-a164-0145-f047-3e459006b046>
2010-12-17 07:58:12 +0000

    Changed: - Font (is now the pokemon font from RBY - Soon-to-be function for key items in KeyboardHandler.cs (is more accurate)
    Added:
    - FPS counter in top left
    
    Removed:
    - Useless Debug info

--- trunk/IAPL_Engine/IAPL_Engine/IAPL_Engine/Input/KeyboardHandler.cs
+++ trunk/IAPL_Engine/IAPL_Engine/IAPL_Engine/Input/KeyboardHandler.cs
@@ -26,28 +26,27 @@ namespace IAPL_Engine
 
             //  if (Keyboard.GetState().IsKeyDown(Keys.Space)) <-- Interact/Select
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Up)) //move up
+            if (Keyboard.GetState().IsKeyDown(Keys.Up)) //  move up
                 if (!player.isMoving)
                         player.StartMove("UP");
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Down)) //move down
+            if (Keyboard.GetState().IsKeyDown(Keys.Down)) //    move down
                 if (!player.isMoving)
                         player.StartMove("DOWN");
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Left)) //move left
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) //    move left
                 if (!player.isMoving)
                         player.StartMove("LEFT");
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Right)) // move right
+            if (Keyboard.GetState().IsKeyDown(Keys.Right)) //   move right
                 if (!player.isMoving)
                         player.StartMove("RIGHT");
 
-            /* if (keyboard.getstate().iskeydown(keys.rightshift)) //Right shift. equivalent of 'select' in the games. -bigplrbear
-                    if (!keyitem.registered) //Also, some example usage. herp derp
-                        console.writeline "Nothing is registered for the shift key \n";
-             *      else if (keyitem.registered)
-             *          [insert over 9000 while/if/for loops here for key items]
-             * */
+            //if(Keyboard.GetState().IsKeyDown(Keys.RightShift)) <-- Use Key Item
+                //if(!keyitem.registered)
+                    //messagebox.send("No key item is registered for that key!");
+                //else
+                    //logic for key item stuff
 
             player.ProcessMovements();
         }
