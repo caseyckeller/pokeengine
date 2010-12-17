2a49f2746fdb9142db84f78ea48d5108888a472b
darkkal999 <darkkal999@a0484e9e-a164-0145-f047-3e459006b046>
2010-12-17 05:16:57 +0000

    Refactored input into the KeyboardHandler class located in pokeengine/IAPL_Engine/IAPL_Engine/IAPL_Engine/Input/KeyboardHandler.cs

---
+++ trunk/IAPL_Engine/IAPL_Engine/IAPL_Engine/Input/KeyboardHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace IAPL_Engine
+{
+    class KeyboardHandler
+    {
+
+        Player player;
+
+        public KeyboardHandler(Player inPlayer)
+        {
+            player = inPlayer;
+        }
+
+        /// <summary>
+        /// Handles all key events upon player input (ran each time update is called in Game1
+        /// </summary>
+        public void ProcessKeyboard()
+        {
+
+            //  if (Keyboard.GetState().IsKeyDown(Keys.Enter)) <-- Enter/Exit Menu
+
+            //  if (Keyboard.GetState().IsKeyDown(Keys.Space)) <-- Interact/Select
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up)) //move up
+                if (!player.isMoving)
+                        player.StartMove("UP");
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down)) //move down
+                if (!player.isMoving)
+                        player.StartMove("DOWN");
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) //move left
+                if (!player.isMoving)
+                        player.StartMove("LEFT");
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right)) // move right
+                if (!player.isMoving)
+                        player.StartMove("RIGHT");
+
+
+            player.ProcessMovements();
+        }
+
+    }
+}
