58f3907b2384f749212eae58d65d7bb1e3e48a31
whatsapokemon <whatsapokemon@a0484e9e-a164-0145-f047-3e459006b046>
2010-12-21 00:45:58 +0000

    Few changes
    Just noticed that pokemon doesn't have a way to evolve, need to add that.

---
+++ trunk/IAPL_Engine/MapEditor/Floors/FloorsList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using IAPL.Map;
+
+namespace MapEditor
+{
+    public class FloorsList
+    {
+        public List<FloorData> floor;
+
+        public FloorsList()
+        {
+            floor = new List<FloorData>();
+            string rootDir = AppDomain.CurrentDomain.BaseDirectory;
+            string floorsDir = Path.Combine(rootDir, "Resources\\Floors");
+
+            foreach (string path in Directory.GetFiles(floorsDir))
+            {
+                if(path.EndsWith(".png"))
+                {
+                    FloorData temp;
+                    temp.image = new Bitmap(path);
+                    temp.tile = new Tile();
+                    floor.Add(temp);
+                }
+            }
+        }
+    }
+
+    public struct FloorData
+    {
+        public Tile tile;
+        public Bitmap image;
+    }
+}
---
+++ trunk/IAPL_Engine/MapEditor/Floors/floorControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using IAPL.Map;
+
+namespace MapEditor
+{
+    /// <summary>
+    /// This displays and retrieves information about floor types
+    /// </summary>
+    public partial class floorControl : UserControl
+    {
+        Tile tile;
+
+        public floorControl(Tile inTile, Bitmap inImage)
+        {
+            InitializeComponent();
+            pictureBox1.Image = inImage;
+            tile = inTile;
+        }
+    }
+}
